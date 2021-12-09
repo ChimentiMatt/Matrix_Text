1b2f58e1295ecfd46911c5494c94dcf2d1669933
matthew chimenti <matthewosu@gmail.com>
2021-12-08 21:51:14 -0800

    Added color to first word and other visual improvments

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let symbols = '/[!@#$%^&*()_+\-=\[\]{};:"\\|,.<>\/?]+/∀8∁∂∃∄∅∆∇∈∉∊∋∌∍∎∏∐∑−∓∔∕∖∗∘∙√∛∜∝∞∟∠∡∢∣∤∥∦∧∨∩∪∫∬∭∮∯∰∱∲∳∴∵∶∷∸∹∺∻∼∽∾∿≀≁≂≃≄≅≆≇≈≉≊≋≌≍≎≏≐≑≒≓≔≕≖≗≘≙≚≛≜≝≞≟≠≡≢≣≤≥≦≧≨≩≪≫≬≭≮≯≰≱≲≳≴≵≶≷≸≹≺≻≼≽≾≿⊀⊁⊂⊃⊄⊅⊆⊇⊈⊉⊊⊋⊌⊍⊎⊏⊐⊑⊒⊓⊔⊕⊖⊗⊘⊙⊚⊛⊜⊝⊞⊟⊠⊡⊢⊣⊤⊥⊦⊧⊨⊩⊪⊫⊬⊭⊮⊯⊰⊱⊲⊳⊴⊵⊶⊷⊸⊹⊺⊻⊼⊽⊾⊿⋀⋁⋂⋃⋄⋅⋆⋇⋈⋉⋊⋋⋌⋍⋎⋏⋐⋑⋒⋓⋔⋕⋖⋗⋘⋙⋚⋛⋜⋝⋞⋟⋠⋡⋢⋣⋤⋥⋦⋧⋨⋩⋪⋫⋬⋭⋮⋯⋰⋱⋲⋳⋴⋵⋶⋷⋸⋹⋺⋻⋼⋽⋾⋿'
+let symbols = '/[!@#$%^&*()_+\-=\[\]{};:"\\|,.<>\/?]+/∀8∁∂∃∄∅∆∇∈∉∊∋∌∍∎∏∐∑−∓∔∕∖∗∘∙√∛∜∝∞∟∠∡∢∣∤∥∦∧∨∩∪∫∬∭∮∯∱∲∳∴∵∶∷∸∹∺∻∼∽∾∿≀≁≂≃≄≅≆≇≈≉≊≋≌≍≎≏≐≑≒≓≔≕≖≗≘≙≚≛≜≞≟≠≡≢≣≤≥≦≧≨≩≪≫≬≭≮≯≰≱≲≳≴≵≶≷≸≹≺≻≼≽≾≿⊀⊁⊂⊃⊄⊅⊆⊇⊈⊉⊊⊋⊌⊍⊎⊏⊐⊑⊒⊓⊔⊕⊖⊗⊘⊙⊚⊛⊜⊝⊞⊟⊠⊡⊢⊣⊤⊥⊦⊧⊨⊩⊪⊫⊬⊭⊮⊯⊰⊱⊲⊳⊴⊵⊶⊷⊸⊹⊺⊻⊼⊽⊾⊿⋀⋁⋂⋃⋄⋅⋆⋇⋈⋉⋊⋋⋌⋍⋎⋏⋐⋑⋒⋓⋔⋕⋖⋗⋘⋙⋚⋛⋜⋝⋞⋟⋠⋡⋢⋣⋤⋥⋦⋧⋨⋩⋪⋫⋬⋭⋮⋯⋰⋱⋲⋳⋴⋵⋶⋷⋸⋹⋺⋻⋼⋽⋾⋿'
 
 let alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 let allChars = symbols + alpha
@@ -14,27 +14,36 @@ let newLetter = document.getElementById('testWord')
 let appBody = document.getElementById('app')
 let opNum = 0
 
+let visibility
+
+let min = Math.ceil(0);
+let max = Math.floor(symbols.length);
+let oneRandomChar = Math.floor(Math.random() * (max - min) + min);
+
+
 // make the string of all characters into a list
 for (let i = 0; i < allChars.length; i++){
     allArray.push(allChars[i])
 }
 
-
-
 // Add random string to HTML Body
 function randomChar() {
  
-        for (let i = 0; i < 300; i ++){
-            console.log(counter)
+        for (let i = 0; i < 350; i ++){
+            // console.log(counter)
+            
+
             // makes a random string of characters
             for (let x = 0; x < 20; x ++){
+                oneRandomChar = Math.floor(Math.random() * (max - min) + min);
                 randomStr += allArray[Math.floor(Math.random()*allArray.length)] + '<br />';
             }
-            appBody.innerHTML += '<div id='+counter+'>'+randomStr+'</div>'
+            appBody.innerHTML += '<div id='+counter+'>'+randomStr+'<span class="lastLetter">'+symbols[oneRandomChar]+'</span></div>'
+
             counter += 1
             randomStr= ''
         }
-        for (let i = 0; i < 300; i++){
+        for (let i = 0; i < 350; i++){
             min = Math.ceil(-5);
             max = Math.floor(110);
             randomHorizontal = Math.random() * (110 + -5) + -5;
@@ -44,7 +53,7 @@ function randomChar() {
             // randomStartingY = Math.random() * (300 +100) - 100
             randomDelay = Math.random() * (35 + 0 ) - 0
 
-            console.log(randomDuration ,'rd')
+            // console.log(randomDuration ,'rd')
             if (randomDuration <= 27){
                 closeness = 8
                 opNum = .2
@@ -72,29 +81,43 @@ function randomChar() {
      
             idName = document.getElementById(i)
             idName.style.position = 'absolute'
-            idName.style.top = '-60vh'
+            idName.style.top = '-65vh'
             animate()    
         }
-
-    
 }
-// setInterval(randomChar, 1000)
-randomChar()
 
 
 function animate(){
     var tl = gsap.timeline({repeat: -1});
     
     tl.to(idName, {delay: 0, fontSize: closeness, duration: 0, x: randomHorizontal+'vw'})
-    tl.to(idName, {delay: randomDelay, color: 'green', y: '210vh', duration: randomDuration, opacity: opNum, ease: 'none'})
+    tl.to(idName, {delay: randomDelay, y: '210vh', duration: randomDuration, opacity: opNum, ease: 'none'})
 }
 
+// let introCounter = 0
+// let introId = 0
+// let randomIntroStr = ''
+
+// function makeIntro () {
+//     // makes a random string of characters
+//     for (let x = 0; x < 200; x ++){
+//         randomIntroStr += allArray[Math.floor(Math.random()*allArray.length)] ;
+//     }
+//     appBody.innerHTML += '<div id='+introCounter+'>'+randomIntroStr+'</div>'
+//     introCounter += 1
+
+//     randomIntroStr= ''
+// }
+
+
+// document.getElementById('#wakeUp').innerHTML = ''
+
+
+gsap.to('#wakeUp', {delay: 0, duration: 2.5, opacity: .8})
+gsap.to('#wakeUp', {delay: 8, duration: 2.5, opacity: 0})
+randomChar()
 
 
 
 
-
-
-
-// setInterval(randomChar, 100)
 
